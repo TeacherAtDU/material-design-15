a14d7f223d0c1622292c278c7035d431d2b6c5ae
sohaib.qadir23@gmail.com <sohaib.qadir23@gmail.com>
2024-02-20 21:58:48 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,4 +13,27 @@ class Form1(Form1Template):
   def submit_button_click(self, **event_args):
     # Display a popup that says 'You clicked the button'
     alert("You clicked the button")
-    # Any code you write here will run before the form opens.
+    # Any code you write here will run before the form opens
+    
+    # Set 'name' to the text in the 'name_box'
+    name = self.name_box.text
+    # Set 'email' to the text in the 'email_box'
+    email = self.email_box.text
+    # Set 'feedback' to the text in the 'feedback_box'
+    feedback = self.feedback_box.text
+
+    # Call your 'add_feedback' server function
+    # pass in name, email and feedback as arguments
+    anvil.server.call('add_feedback', name, email, feedback)
+
+    # Show a popup that says 'Feedback submitted!'
+    Notification("Feedback submitted!").show()
+
+    # Call your 'clear_inputs' method to clear the boxes
+    self.clear_inputs()
+
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
